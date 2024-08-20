810e2650e18c45e9fb1aafdb9921090209f05f52
Pedro Tomás dos Santos <pedro.tomas.santos@celfocus.com>
2024-08-20 12:13:10 +0100

    Add base navigation

--- frontend/src/components/Sidebar/Sidebar.tsx
+++ frontend/src/components/Sidebar/Sidebar.tsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
-  return <aside className="border-r border-secondary w-64 p-4"></aside>;
+  return (
+    <aside className="border-r border-secondary w-64 p-4">
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Dashboard</Link>
+          </li>
+          <li>
+            <Link to="/expenses">Expenses</Link>
+          </li>
+          <li>
+            <Link to="/income">Income</Link>
+          </li>
+          <li>
+            <Link to="/reports">Reports</Link>
+          </li>
+          <li>
+            <Link to="/settings">Settings</Link>
+          </li>
+        </ul>
+      </nav>
+    </aside>
+  );
 };
 
 export default Sidebar;
