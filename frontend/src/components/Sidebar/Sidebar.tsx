8599eafabf4fab7b4a08dddb56e1f75078edf334
Pedro Tomás dos Santos <pedro.tomas.santos@celfocus.com>
2024-08-20 12:39:44 +0100

    Update styles

--- frontend/src/components/Sidebar/Sidebar.tsx
+++ frontend/src/components/Sidebar/Sidebar.tsx
@@ -1,32 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Sidebar = () => {
   const navigate = useNavigate();
-  const styles =
-    "w-auto text-center text-sky-500 p-4 border-b border-secondary text-xl cursor-pointer hover:bg-secondary hover:text-white uppercase";
+  const [selected, setSelected] = useState<string>("/");
+
+  const styles = {
+    base: "w-auto text-center text-sky-500 p-4 border-b border-secondary text-xl cursor-pointer hover:bg-secondary hover:text-white uppercase",
+    selected:
+      "w-auto text-center text-white p-4 border-b border-secondary text-xl cursor-pointer bg-secondary uppercase",
+  };
+
+  ("w-auto text-center text-white p-4 border-b border-secondary text-xl cursor-pointer bg-secondary uppercase");
 
   const handleClick = (path: string) => {
+    setSelected(path);
     navigate(path);
   };
-  
+
   return (
     <aside className="border-r border-secondary w-64 ">
       <nav>
         <ul className="">
-          <li className={styles} onClick={() => handleClick('/')}>
-            <h1>Dasboard</h1>
+          <li
+            className={selected === "/" ? styles.selected : styles.base}
+            onClick={() => handleClick("/")}
+          >
+            <h1>Dashboard</h1>
           </li>
-          <li className={styles} onClick={() => handleClick('/expenses')}>
+          <li
+            className={selected === "/expenses" ? styles.selected : styles.base}
+            onClick={() => handleClick("/expenses")}
+          >
             <h1>Expenses</h1>
           </li>
-          <li className={styles} onClick={() => handleClick('/income')}>
+          <li
+            className={selected === "/income" ? styles.selected : styles.base}
+            onClick={() => handleClick("/income")}
+          >
             <h1>Income</h1>
           </li>
-          <li className={styles} onClick={() => handleClick('/reports')}>
+          <li
+            className={selected === "/reports" ? styles.selected : styles.base}
+            onClick={() => handleClick("/reports")}
+          >
             <h1>Reports</h1>
           </li>
-          <li className={styles} onClick={() => handleClick('/settings')}>
+          <li
+            className={selected === "/settings" ? styles.selected : styles.base}
+            onClick={() => handleClick("/settings")}
+          >
             <h1>Settings</h1>
           </li>
         </ul>
